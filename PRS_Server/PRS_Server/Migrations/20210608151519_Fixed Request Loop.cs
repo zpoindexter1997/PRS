19f657b3f86a2ba43b1249bb88e88b1e8b523cbb
zpoindexter1997 <zpoindexter97@gmail.com>
2021-06-28 15:24:35 -0400

    Added migrations

---
+++ PRS_Server/PRS_Server/Migrations/20210608151519_Fixed Request Loop.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PRS_Server.Migrations
+{
+    public partial class FixedRequestLoop : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_RequestLines_RequestId",
+                table: "RequestLines");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RequestLines_RequestId",
+                table: "RequestLines",
+                column: "RequestId",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_RequestLines_RequestId",
+                table: "RequestLines");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RequestLines_RequestId",
+                table: "RequestLines",
+                column: "RequestId");
+        }
+    }
+}
